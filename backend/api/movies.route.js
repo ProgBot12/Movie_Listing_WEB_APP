902d4c45fc82fa5468f8e25a040a9e49e8f603f4
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-06 15:20:09 -0500

    Create movies.route.js

---
+++ backend/api/movies.route.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router(); //get access to express router
+
+router.route("/").get((req, res) => res.send("Hello World"));
+
+export default router;
