00220a7acc0669cc2434c7106d6221daa393e748
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-08 12:55:44 -0500

    Update reviewsDAO.js

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -18,4 +18,51 @@ export default class ReviewsDAO {
       );
     }
   }
+
+  static async addReview(movieId, user, review, date) {
+    try {
+      const reviewDoc = {
+        name: user.name,
+        user_id: user._id,
+        date: date,
+        review: review,
+        movie_id: ObjectId(movieId),
+      };
+      return await reviews.insertOne(reviewDoc);
+    } catch (error) {
+      console.error(`unable to post review: ${error}`);
+      return { error: error };
+    }
+  }
+
+  static async updateReview(reviewId, userId, review, date) {
+    try {
+      const updateResponse = await reviews.updateOne(
+        {
+          user_id: userId,
+          _id: ObjectId(reviewId),
+        },
+        {
+          $set: { review: review, date: date },
+        }
+      );
+      return updateResponse;
+    } catch (error) {
+      console.error(`unable to update review: ${error}`);
+      return { error: error };
+    }
+  }
+
+  static async deleteReview(reviewId, userId) {
+    try {
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId(reviewId),
+        user_id: userId,
+      });
+      return deleteResponse;
+    } catch (error) {
+      console.error(`unable to delete review: ${error}`);
+      return { error: error };
+    }
+  }
 }
