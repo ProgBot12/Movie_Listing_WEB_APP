d164f64dd93a3b4d69afeb740f983fbfdca209d8
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-08 12:17:15 -0500

    added movies controller

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const router = express.Router(); //get access to express router
 
-router.route("/").get((req, res) => res.send("Hello World"));
+router.route("/").get((req, res) => res.send("Hello World!"));
 
 export default router;
