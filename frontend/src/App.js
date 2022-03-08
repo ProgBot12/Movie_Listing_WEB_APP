75fc977f70f86741861e2078824d5af16d790eba
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-08 18:11:59 -0500

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Router, Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import AddReview from "./components/AddReview";
+import MoviesList from "./components/MoviesList";
+import Movie from "./components/Movie";
+import Login from "./components/Login";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <button onClick={logout}>Logout User</button>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path={["/", "/movies"]}
+            component={<MoviesList />}
+          ></Route>
+          <Route
+            path="/movies/:id/review"
+            render={(props) => <AddReview {...props} user={user} />}
+          ></Route>
+          <Route
+            path="movies/:id"
+            render={(props) => <Movie {...props} user={user} />}
+          ></Route>
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} login={login} />}
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
