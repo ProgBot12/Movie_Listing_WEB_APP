a6c006474bfe4c5f38b9b008c1a7e5e1a0822c4d
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-08 19:36:42 -0500

    Update MoviesList.jsx

--- frontend/src/components/MoviesList.jsx
+++ frontend/src/components/MoviesList.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
 
-function MoviesList() {
+function MoviesList(props) {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState("");
+  const [ratings, setRatings] = useState(["All Ratings"]);
+
+  useEffect(() => {
+    retrieveMovies();
+    retrieveRatings();
+  });
+
+  //Retrieve movies
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  //Retrieve ratings
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then((response) => {
+        console.log(response.data);
+        setRatings(["All Ratings"].concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return <div className="App">Movies List</div>;
 }
 
